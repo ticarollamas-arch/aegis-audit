686eb443fc241cb82e24d94cbb186743cd634590
tianacarolinelamas <ticarollamas@gmail.com>
2026-07-26 11:44:10 -0300

    Commit inicial do Blueprint: aegis-audit/cli/main.py

---
+++ aegis-audit/cli/main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# main.py
+# Ponto de entrada da aplicação, menu interativo e orquestração dos comandos.
+
+print("Inicializando subsistema: main.py")
