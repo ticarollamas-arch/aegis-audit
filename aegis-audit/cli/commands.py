c046ef4c921b5b367df5502cfb4127dcae3b65a1
tianacarolinelamas <ticarollamas@gmail.com>
2026-07-26 11:44:13 -0300

    Commit inicial do Blueprint: aegis-audit/cli/commands.py

---
+++ aegis-audit/cli/commands.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# commands.py
+# Implementação lógica dos comandos do menu (Recon, Enum, Audit, etc.).
+
+print("Inicializando subsistema: commands.py")
